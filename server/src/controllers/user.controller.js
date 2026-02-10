b15a1475d72d6896abe8bce830f6baa8224a55c2
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-10 12:23:55 +0530

    Implement user registration controller with email validation and token generation

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,38 @@
+import User from "../models/user.model.js";
+import { hashPassword, generateToken } from "../utils/index.js";
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User with this email already present",
+        data: {},
+      });
+    }
+    password = await hashPassword(req.body.password);
+    user = await User.create({ name, email, password });
+    const token = await generateToken({ id: user._id });
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(201).json({
+      message: "User registered successfully",
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error", data: {} });
+  }
+};
+
+export default {
+  register,
+};
