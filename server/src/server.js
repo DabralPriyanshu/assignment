9ba513a3851bcfa5b41fe29a8a38bcc2362dad50
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-10 11:42:15 +0530

    Added middleware to parse JSON and URL-encoded request bodies

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import ENV from "./config/server.config.js";
 import connectDB from "./config/db.config.js";
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.listen(ENV.PORT, async () => {
   await connectDB();
   console.log(`Server started at port ${ENV.PORT}`);
