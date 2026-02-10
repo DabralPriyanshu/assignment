a9ef7a8ab1dcf864a25766da77d24d1bfcf1108f
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-10 12:24:28 +0530

    Add user route for registration endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import ENV from "./config/server.config.js";
 import connectDB from "./config/db.config.js";
+import userRoutes from "./routes/user.route.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/users", userRoutes);
 app.listen(ENV.PORT, async () => {
   await connectDB();
   console.log(`Server started at port ${ENV.PORT}`);
